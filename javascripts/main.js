793c979a4c954a92e09f99e6206a36bb1ac4093d
Federico Sintucci <fsintucci@gmail.com>
2013-12-10 14:22:23 +0100

    FIX white bg on overflow shown

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,24 @@
 function setFullHeight() {
-	if ($('.column').height() < $(document).height()){
-		$('.column').css('height', $(document).height()+'px');
+	var docHeight = $(document).height();
+	if ($('.column').height() < docHeight){
+		$('.column').css('height', docHeight +'px');
 	}	
 }
 
-$(function() {
+function equalizeHeight() {
+	$('.code-area').each(function(){
+		$this = $(this);
+		var height = $this.height();
+		if(height > 1 && $this.siblings(".desc-area").height() < height) {
+			var sumHeight = height + 80;
+			$this.siblings(".desc-area").css("height", sumHeight);
+		}
+	});
+}
 
-	setFullHeight();
+
+$(function() {
 	$('.collapse').collapse();
+	equalizeHeight();
+	setFullHeight();
 });
