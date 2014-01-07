80705b7c8806901d2489c5c78dfc6e43eac5d8ea
Federico Sintucci <fsintucci@gmail.com>
2014-01-07 14:29:56 +0100

    Unused js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,24 +1,3 @@
-function setFullHeight() {
-	if($(window).width() > 768) {
-		var docHeight = $(document).height();
-		if ($('.column').height() < docHeight){
-			$('.column').css('height', docHeight +'px');
-		}	
-	}
-}
-
-function equalizeHeight() {
-	$('.code-area').each(function(){
-		$this = $(this);
-		var height = $this.height();
-		if(height > 1 && $this.siblings(".desc-area").height() < height) {
-			var sumHeight = height + 80;
-			$this.siblings(".desc-area").css("height", sumHeight);
-		}
-	});
-}
-
-
 function accordionToggle() {
 	$('#accordion div').on('show.bs.collapse', function () {
 		$(this).find("span").removeClass('glyphicon-collapse-down').addClass('glyphicon-collapse-up');
@@ -28,19 +7,10 @@ function accordionToggle() {
 	});
 }
 
-
-
-
-
-
 $(function() {
 
 	$('.collapse').collapse();
 	//console.log(window._collapse);
-	
-	equalizeHeight();
-
-	setFullHeight();
 
 	accordionToggle();
 	
